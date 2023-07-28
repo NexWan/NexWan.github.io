d5fa510beb9a2213f77e00a5c1135a6010759a7a
NexWan <l21051403@saltillo.tecnm.mx>
2023-07-28 08:09:30 -0600

    fixed scroll not updating menu tab

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
-new fullpage("#fullpage", {
-    sectionsColor: ['#32a852', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    anchors: ["section1", "section2", "section3"],
-    controlArrows: false,
-    navigation: true,
-    slidesNavigation: true,
-    menu: '#header'
+document.addEventListener("DOMContentLoaded", function () {
+    new fullpage("#fullpage", {
+      sectionsColor: ['#32a852', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+      anchors: ["section1", "section2", "section3"],
+      controlArrows: false,
+      navigation: true,
+      slidesNavigation: true,
+      menu: '#menu', // Updated the menu option to use '#menu' since it's the ID of your menu list.
+      afterLoad: function (origin, destination, direction) {
+        // Remove the 'active' class from all menu items
+        document.querySelectorAll("#menu li").forEach(function (item) {
+          item.classList.remove("active");
+        });
+  
+        // Add the 'active' class to the corresponding menu item based on the section anchor
+        var menuItems = document.querySelectorAll("#menu li");
+        var activeMenuItem = document.querySelector(`#menu li[data-menuanchor="${destination.anchor}"]`);
+        if (activeMenuItem) {
+          activeMenuItem.classList.add("active");
+        }
+      },
+    });
   });
-
-  var toggle = ['link1','link2','link3']
-
-  for (let index = 0; index < toggle.length; index++) {
-        console.log(`'${toggle[index]}'`);
-        let element = document.getElementById(`${toggle[index]}`)
-        console.log(element);
-        element.addEventListener('click', ()=>{
-            var elementId = element.id
-            console.log(elementId);
-            switchTab(elementId);
-        })
-  }
-
-  function switchTab(id){
-    if(document.getElementById(id).classList.contains("active")) return
-
-    document.querySelector('li.active').classList.remove('active')
-    document.getElementById(id).parentNode.classList.add('active')
-  }
+  
