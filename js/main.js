9c2e6ea21dffd8a4a777ca3342e1de8bfb3cfa21
NexWan <l21051403@saltillo.tecnm.mx>
2023-07-29 05:24:58 -0600

    Added several stuff: - Removed fullpage.js and fullpage.css, addeed the CDN versions instead - Added the main section and a about me section, currently working on the works section - added some comments -added a image -added particles.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,158 @@
+// Wait for the content to be loaded, it holds the config of the fullpage library
 document.addEventListener("DOMContentLoaded", function () {
     new fullpage("#fullpage", {
-      sectionsColor: ['#32a852', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+      sectionsColor: ['none', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
       anchors: ["section1", "section2", "section3"],
       controlArrows: false,
+      normalScrollElements: '.mainContent',
       navigation: true,
       slidesNavigation: true,
-      menu: '#menu', // Updated the menu option to use '#menu' since it's the ID of your menu list.
-      afterLoad: function (origin, destination, direction) {
-        // Remove the 'active' class from all menu items
+      menu: '#menu',
+      afterLoad: function (origin, destination, direction) {  //This function works as the handler of the active nav, for some reason the default nav handler doesnt work for me
         document.querySelectorAll("#menu li").forEach(function (item) {
           item.classList.remove("active");
         });
-  
-        // Add the 'active' class to the corresponding menu item based on the section anchor
         var menuItems = document.querySelectorAll("#menu li");
         var activeMenuItem = document.querySelector(`#menu li[data-menuanchor="${destination.anchor}"]`);
         if (activeMenuItem) {
           activeMenuItem.classList.add("active");
         }
       },
-    });
+    })
   });
+
+  // Particles js config
+
+  /* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
   
+{
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#b61924",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}
+
+);
